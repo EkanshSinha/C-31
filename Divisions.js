19cb4c1c5e6ff97b6fc153a35f09aaf50bdf50f1
EkanshSinha <71957034+EkanshSinha@users.noreply.github.com>
2021-01-24 12:56:56 +0530

    Add files via upload

---
+++ Divisions.js
@@ -0,0 +1,19 @@
+class Divisions {
+constructor(x,y,x,h){
+var options ={
+
+    inStatic: true
+}
+this.body= Bodies.rectangle(x,y,w,h,options);
+this.w=w;
+this.h=h;
+World.add(world,this.body);
+}
+display(){
+var pos = this.body.position;
+rectmode(CENTER);
+fill("white");
+rect(pos.x,pos.y,this.w,this.h)
+
+  }
+};
